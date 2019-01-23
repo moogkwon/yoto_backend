f2224c664f48b8593a7aaedafdd9e7ae332750b0
Duy Luong <duyluonglc@gmail.com>
2019-01-23 16:47:07 +0700

    remove beta

--- beta/application/views/elements/footer.php
+++
@@ -1,6 +0,0 @@
-	<footer class="app-footer">
-            <a href="#" class="text-theme"><?php echo SITE_TITLE; ?></a> &copy; 2018 <?php echo SITE_TITLE; ?>.
-    </footer>
-	<?php $this->load->view('elements/footer_scripts'); ?>
-	</body>
-	</html>
--- beta/application/views/elements/left_sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php
- if($this->session->userdata('user_type')=='admin'){ 
-	$this->load->view('elements/left_sidebar_admin');
- }else{
-	$this->load->view('elements/left_sidebar_dealer');
- }
